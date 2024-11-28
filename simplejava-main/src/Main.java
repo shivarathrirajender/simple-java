220bbf09889987ec7c4ada36b48d797b500d90a0
durga <thotadurga2464@gmail.com>
2024-11-28 05:46:13 +0530

    first commit

---
+++ simplejava-main/src/Main.java
@@ -0,0 +1,12 @@
+// File: src/Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        // Simple calculation: addition of two numbers
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is " + sum);
+    }
+}
